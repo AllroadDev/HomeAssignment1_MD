d236c1fec09e87a2064b94644de04e051cd5b353
AllroadDev <sasha110102@gmail.com>
2019-10-09 11:47:54 +0300

    minShape and maxShape out of all

--- src/com/mainacad/ApplicationRunner.java
+++ src/com/mainacad/ApplicationRunner.java
@@ -8,9 +8,43 @@ import com.mainacad.abs.Shape;
 public class ApplicationRunner {
     public static void main(String[] args) {
 
-        Shape triangle = new Triangle();
-        Shape circle = new Circle();
-        Shape square = new Square();
+        Triangle triangle = new Triangle();
+        triangle.setBase(13.5);
+        triangle.setHeights(10.0);
+
+        Circle circle = new Circle();
+        circle.setRadius(7.5);
+
+        Square square = new Square();
+        square.setSide(45.0);
+
+        Shape maxShape;
+        Shape minShape;
+
+        if (circle.getArea() > square.getArea()) {
+        maxShape = circle;
+
+        } else {
+            maxShape = square;
+        }
+        if (maxShape.getArea() < triangle.getArea()) {
+            maxShape = triangle;
+        }
+        System.out.println(" Max shape is " + maxShape.getClass().getSimpleName() + " and has area " + maxShape.getArea());
+
+
+        if (circle.getArea() > square.getArea()) {
+            minShape = circle;
+
+        } else {
+            minShape = square;
+        }
+
+        if (minShape.getArea() > triangle.getArea()) {
+            minShape = triangle;
+
+        }
+        System.out.println(" Min shape is " + minShape.getClass().getSimpleName() + " and has area " + minShape.getArea() );
 
 //        Square square = new Square();
 //        square.setSide(10.0);
