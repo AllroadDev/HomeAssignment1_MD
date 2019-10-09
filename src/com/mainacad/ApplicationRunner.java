2f1e11cfb0afa9e793ca937023e11da98f4d9c2f
AllroadDev <sasha110102@gmail.com>
2019-10-09 11:32:03 +0300

    Add abstraction

--- src/com/mainacad/ApplicationRunner.java
+++ src/com/mainacad/ApplicationRunner.java
@@ -1,5 +1,27 @@
 package com.mainacad;
 
-public class ApplicationRunner {
+import com.mainacad.Circle.Circle;
+import com.mainacad.Square.Square;
+import com.mainacad.Triangle.Triangle;
+import com.mainacad.abs.Shape;
 
+public class ApplicationRunner {
+    public static void main(String[] args) {
+
+        Shape triangle = new Triangle();
+        Shape circle = new Circle();
+        Shape square = new Square();
+
+//        Square square = new Square();
+//        square.setSide(10.0);
+////        square.setName(" Square ");
+//
+//
+//        System.out.println(" Square with side " + square.getSide() + " has area " + square.getArea() );
+//
+//        Circle circle = new Circle();
+//        circle.setRadius(14.0);
+//
+//        System.out.println(" Circle with radius " + circle.getRadius() + " has area " + circle.getArea());
+    }
 }
